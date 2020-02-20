f85493195f0fe62d2f233666b61febf732398bb7
SqueezyDough <lvanbiljouw@hotmail.com>
2020-02-20 15:25:16 +0100

    Delete unnecessary files

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,31 @@
 
-let xhr = new XMLHttpRequest();
+import * as templateEngine from "./templateEngine.js";
 
-xhr.open("GET", "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards", true);
-xhr.setRequestHeader("x-rapidapi-host", "omgvamp-hearthstone-v1.p.rapidapi.com");
-xhr.setRequestHeader("x-rapidapi-key", "7df4822919mshbffd471d5b9df6fp1a2b0bjsndd91db79cbcd");
-xhr.send();
+fetchData();
 
-xhr.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-        console.log(this.responseText);
-    }
-};
+function fetchData() {
+    const url = 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards';
+
+    fetch(url, {
+        headers: {
+            'x-rapidapi-host' : 'omgvamp-hearthstone-v1.p.rapidapi.com',
+            'x-rapidapi-key' : '7df4822919mshbffd471d5b9df6fp1a2b0bjsndd91db79cbcd'
+        }
+    }).then( res => { if (res.ok) { return res.json() }
+    }).then(data => {
+        console.log(data);
+
+        const explorerContainer = document.getElementById('explorer');
+
+        data.Classic.forEach(card => {
+            if (card.img) {
+                const cardImage = document.createElement('img');
+                cardImage.setAttribute('src', card.img);
+                explorerContainer.appendChild(cardImage);
+            }
+        });
+
+        //const explorerContainer = document.getElementById('explorer')
+        //const renderHTML = templateEngine.renderView(explorerHTML, data);
+    });
+}
